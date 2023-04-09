b77e8a65f95bb42a2af9a1940a50aee3b2345a63
root <2059997196@qq.com>
2023-04-09 17:16:32 +0800

    second commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -3,5 +3,6 @@ package git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
