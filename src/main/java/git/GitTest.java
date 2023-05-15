5f2accbd4df4bb5a7c50eeece05a5c91484db275
chenqin <490358423@qq.com>
2023-05-15 21:19:10 +0800

    master complete

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -9,8 +9,7 @@ public class GitTest {
         System.out.println("hot-fix forth commit");
         System.out.println("push test"); 
         System.out.println("pull test");
+        //master complete
     }
 }
 
-git 777
-feature
