f47b1f3c09816d42719bb8075aa5156a69e1c898
chenqin <490358423@qq.com>
2023-05-15 21:08:53 +0800

    test 3

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
         System.out.println("third commit");
         System.out.println("master forth commit");
         System.out.println("hot-fix forth commit");
-        System.out.println("push test"); test
+        System.out.println("push test"); 
         System.out.println("pull test");
     }
 }
