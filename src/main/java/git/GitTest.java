89711bd8b10a648bd04f2a6b2cdae41ec03eeb51
chenqin <490358423@qq.com>
2023-05-15 21:39:24 +0800

    debug 1

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -2,16 +2,7 @@ package git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("first commit");
-        System.out.println("second commit");
-        System.out.println("third commit");
-        System.out.println("master forth commit");
-        System.out.println("hot-fix forth commit");
-        System.out.println("push test"); 
-        System.out.println("pull test");
-        //master complete
-        //git merge 
-        // debug
+
     }
 }
 
