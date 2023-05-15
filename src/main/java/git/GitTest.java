c87d51a83bdd1d8b6f5e06f18d1f19ab73c9f6e8
chenqin <490358423@qq.com>
2023-05-15 22:34:37 +0800

    fdsf

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -2,14 +2,6 @@ package git;
 
 public class GitTest {
     public static void main(String[] args) {
-        //1
-        //2
-        //3
-        //4
-        //5
-        //6
-
-        //1
         System.out.println("first commit");
         System.out.println("second commit");
         System.out.println("third commit");
@@ -17,7 +9,5 @@ public class GitTest {
         System.out.println("hot-fix forth commit");
         System.out.println("push test");
         System.out.println("pull test");
-        System.out.println("final test")
     }
 }
-
